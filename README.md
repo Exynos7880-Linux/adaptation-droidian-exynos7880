f719de4f25babf71d297fb1e8e295899d543a341
aymanrgab <aymanrar2c@gmail.com>
2022-12-10 18:10:52 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Droidian Adaptation for Exynos7880
+
+## Usage
+
+1. clone the repo;
+```
+git clone https://github.com/Exynos7880-Linux/adaptation-droidian-exynos7880.git
+```
+2. push to device using adb;
+```
+adb push adaptation-droidian-exynos7880/* /tmp/
+```
+3. installing
+```
+adb shell ./tmp/setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/sbin/sh
 echo "Droidian Adaptation for Exynos7880 devices"
 echo "Made by Ayman"
 umount /mnt/
 mount /data/rootfs.img /mnt/
 cd droidian
 cp -r * /mnt/
-chroot /mnt/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin" && systemctl enable samsung-hwc && chmod 755 /var/lib/lxc/android/pr* && dpkg -i /tmp/debs/*.deb && rm -rf /tmp/debs'
+chroot /mnt/ /bin/bash -c 'export PATH="$PATH:/usr/bin:/usr/sbin" && systemctl enable samsung-hwc && systemctl enable apt-fix && chmod 755 /var/lib/lxc/android/pr* && dpkg -i /tmp/debs/*.deb && rm -rf /tmp/debs'
 umount /mnt/
 echo "Done!"
 echo "It is recommended to run these command once Droidian is booted;"
